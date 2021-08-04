bfc957917981e9fd5328ef8ef2639d604a4c2fd4
TnTien <nguyen.tien25@gmail.com>
2021-08-03 18:15:46 -0600

    Added comments to main.js

--- JavaScript Projects/Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
+++ JavaScript Projects/Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
@@ -11,6 +11,8 @@ function currentSlide(n) {
     showSlides(slideIndex = n);
 }
 
+// Will loop through the images and display the current value n is on.
+// Loop through dot and change color 
 function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("mySlides");
@@ -32,12 +34,12 @@ function countdown() {
 
     function tick() {
         seconds = seconds - 1;
-        timer.innerHTML = seconds;
+        timer.innerHTML = seconds; // Display timer and countdown
         var time = setTimeout(tick, 1000);
         if (seconds == -1) {
-            alert("Time's up!");
-            clearTimeout(time);
-            timer.innerHTML = "";
+            alert("Time's up!"); // Pop up window displaying message 
+            clearTimeout(time); // Stops the Timeout, so timer doesn't continue to run
+            timer.innerHTML = ""; // Clears the p element displaying timer
         }
     }
     tick();
